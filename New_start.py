f392608667ae31b7babfb73edd9f56f34aec2176
Cordova520 <A01632734@itesm.mx>
2020-06-27 14:10:14 -0500

    logger_config

--- New_start.py
+++ New_start.py
@@ -7,6 +7,14 @@ from selenium.webdriver.common.action_chains import ActionChains
 import platform 
 import os
 
+#---------------------LOGGER--------------------------------#
+import logging
+import logging.config
+
+logging.config.fileConfig('logging.conf')
+
+logger = logging.getLogger('root')
+
 class  startChrome():
     def __init__(self):
         super().__init__()
@@ -18,12 +26,14 @@ class  startChrome():
             self.driver = webdriver.Chrome()
         else:
             self.driver = webdriver.Chrome("./chrome/chromedriver")
+        logger.warning("This code was excecuted from {}".format(platform.system()))
 
         self.driver.get("https://instagram.com")
         self.username = 'photoandtravel2020'
         self.password = 'mannheimzittau'
         self.user = WebDriverWait(self.driver,10).until(EC.presence_of_element_located((By.NAME, 'username'))).send_keys(self.username)
         self.pas = WebDriverWait(self.driver,10).until(EC.presence_of_element_located((By.NAME, 'password'))).send_keys(self.password + Keys.ENTER)
+        logger.info("Acces Granted")
         WebDriverWait(self.driver, 10).until(EC.presence_of_element_located((By.XPATH, "//img[contains(@alt, 'Instagram')]"))).click()
 
 
